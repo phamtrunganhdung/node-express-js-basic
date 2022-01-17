3808dc1bbc7577dfe0fc741de20e56fe21c58cd1
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2022-01-17 13:50:15 +0700

    RESTfull API

---
+++ express-basic/src/route/api.js
@@ -0,0 +1,13 @@
+import express from "express";
+import APIController from "../controllers/APIController.js";
+let router = express.Router();
+
+const initAPIRoute = (app) => {
+  router.get("/users", APIController.getAllUsers);
+  router.post("/create-new-user", APIController.createNewUser);
+  router.put("/update-user", APIController.updateUser);
+  router.delete("/delete-user/:id", APIController.deleteUser);
+  return app.use("/api/v1", router);
+};
+
+export default initAPIRoute;
--- express-basic/src/server.js
+++ express-basic/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoute from "./route/web.js";
+import initAPIRoute from "./route/api.js";
 require("dotenv").config();
 const app = express();
 const port = process.env.port || 8080;
@@ -11,6 +12,8 @@ app.use(express.json());
 configViewEngine(app);
 
 initWebRoute(app);
+
+initAPIRoute(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
