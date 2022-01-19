6c645600e76fd74a84829e38da16f7a0cf2c151b
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2022-01-19 21:24:30 +0700

    middleware

--- express-basic/src/server.js
+++ express-basic/src/server.js
@@ -5,6 +5,15 @@ import initAPIRoute from "./route/api.js";
 require("dotenv").config();
 const app = express();
 const port = process.env.port || 8080;
+import morgan from "morgan";
+
+app.use((req, res, next) => {
+  console.log(">>> run into my middleware");
+  console.log(req.method);
+  next();
+});
+
+app.use(morgan("combined"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,6 +23,10 @@ configViewEngine(app);
 initWebRoute(app);
 
 initAPIRoute(app);
+
+app.use((req, res) => {
+  return res.render("404.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
